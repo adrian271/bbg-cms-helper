17acafa083e92a82999f334848f9099d3b7002ca
Adrian Barnes <adrian@apexwebdev.com>
2016-08-10 22:51:20 -0700

    Finished for 1 working module, properly represent Photo Left with Text on Right

--- js/main.js
+++ js/main.js
@@ -1,21 +1,45 @@
 $(document).ready(function(){
     var templateEng = function($selection) {
-        var template = $selection.find('script').html();
+        var template = $selection.find('template').html();
         var output = template;
         $selection.find('label').each(function(){
           var replacer = $(this).find('input').val();
           var word = $(this).text();
-          var regexp = new RegExp("{{"+word+"}}","gim");
-          output = output.replace(regexp,replacer);
+          var regExp = new RegExp("{{"+word+"}}","gim");
+          output = output.replace(regExp,replacer);
         });
         return output;
     }
 
     $('label').each(function(){
-        var what = $(this).text();
-        $(this).append('<input id="'+what+'" type="text">');
-        $('input').val('test');
+        var labelName = $(this).text();
+        $(this).append('<input id="'+labelName+'" type="text">');
     });
+    $('section').each(function(){
+        var output = templateEng($(this));
+        //console.log(output)
+        $(this).find('.copy-text').text(output);
+        $(this).find('.live-example').html(output);
+    });
+    $('body').on('keyup','input',function(){
+        var target = $(this).parents('section').eq(0);
+        var changed = templateEng(target);
+        target.find('.copy-text').text(changed);
+        target.find('.live-example').html(changed);
+    });
+
+
+
+
+
+    //Fill with sample dummydata
+    var counter = 0
+    $('#Photo-Left-w-text input').each(function(){
+        $(this).val(dummy[counter]);
+        console.log(dummy[counter]);
+        counter++;
+    });
+
     $('section').each(function(){
         var output = templateEng($(this));
         console.log(output)
